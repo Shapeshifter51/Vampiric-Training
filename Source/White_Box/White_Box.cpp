0b6e557d6cb4db0a0f665a32da0c0f9e32da2fdb
Shapeshifter51 <kyledownie0108@gmail.com>
2024-12-03 13:05:24 +0000

    Revert "continued with the UI"
    This reverts commit 61b3a047a8380dad0dc887007338650b9f233b0f.

--- Source/White_Box/White_Box.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "White_Box.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, White_Box, "White_Box" );
